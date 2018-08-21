f0479d2a197ec60586ee3814eb834953ab7d2388
Fábio Júnior <33606799+JuniorPenguim@users.noreply.github.com>
2018-08-20 21:41:08 -0300

    Update Exercicio_dois.cpp
    Gráficos senoidais em partes

--- Exercicio_dois.cpp
+++ Exercicio_dois.cpp
@@ -3,11 +3,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-void display_6(void);
-void display_12(void);
-void display_180(void);
+void display(void);
 void init (void);
 void keyboard(unsigned char key, int x, int y);
+char var;
 
 int main(int argc, char** argv)
 {
@@ -17,13 +16,13 @@ int main(int argc, char** argv)
    glutInitWindowPosition (100, 100);
    glutCreateWindow ("Exercicio 2 Test");
    init ();
-   glutDisplayFunc(display_6);
+   glutDisplayFunc(display);
    glutKeyboardFunc(keyboard);
    
    
 
 
-	printf("Pressione ESC para fechar.\n");
+   printf("Pressione ESC para fechar.\n");
 
    glutMainLoop();
 
@@ -34,154 +33,156 @@ int main(int argc, char** argv)
 
 // É a rotina chamada automaticamente sempre que a
 // janela ou parte dela precisa ser redesenhada
-void display_6(void)
+void display(void)
 {
 
 
-   // Limpar todos os pixels
-   glClear (GL_COLOR_BUFFER_BIT);
-
-   // Desenhar a linha x do eixo
-    glBegin(GL_LINES);
-
-      glColor3f(1.0, 1.0, 1.0);
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(360.0, 0.0, 0.0);
-
-   glEnd();
-
-   // Desenhar a linha y do eixo
-   glBegin(GL_LINES);
-
-      glColor3f(1.0, 1.0, 1.0);
-      glVertex3f(0.0, -1.0, 0.0);
-      glVertex3f(0.0, 1.0, 0.0);
-
-   glEnd();
-   
-
-      
-   for(float x = 0; x < 360; x = x + 60)
+   switch(var)
    {
-      glBegin(GL_LINE_STRIP);
+      case 'a':
 
-         float y = sin(x*((3.1415)/180));        
-         glColor3f(0.0, 1.0, 0.0);
-         glVertex2f(x, y);
-         glVertex2f((x+60),sin((x+60)*((3.1415)/180)));
+      // Limpar todos os pixels
+      glClear (GL_COLOR_BUFFER_BIT);
 
-         
-         
-         glEnd();
+      // Desenhar a linha x do eixo
+      glBegin(GL_LINES);
 
-      
-
-      glutSwapBuffers ();
-      
-   }
-
-  
-
-}
-
-void display_12(void)
-{
-
-
-   // Limpar todos os pixels
-   glClear (GL_COLOR_BUFFER_BIT);
-
-   // Desenhar a linha x do eixo
-    glBegin(GL_LINES);
-
-      glColor3f(1.0, 1.0, 1.0);
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(360.0, 0.0, 0.0);
-
-   glEnd();
-
-   // Desenhar a linha y do eixo
-   glBegin(GL_LINES);
-
-      glColor3f(1.0, 1.0, 1.0);
-      glVertex3f(0.0, -1.0, 0.0);
-      glVertex3f(0.0, 1.0, 0.0);
-
-   glEnd();
-   
-
-   // Desenhar gráfico senoidal em loop   
-   for(float x = 0; x < 360; x = x + 30)
-   {
-      glBegin(GL_LINE_STRIP);
-
-         float y = sin(x*((3.1415)/180));        
-         glColor3f(0.0, 1.0, 0.0);
-         glVertex2f(x, y);
-         glVertex2f((x+30),sin((x+30)*((3.1415)/180)));
-
-         
-         
-         glEnd();
-
-      
-
-      glutSwapBuffers ();
-      
-   }
-
-  
-
-}
-
-void display_180(void)
-{
-
-
-   // Limpar todos os pixels
-   glClear (GL_COLOR_BUFFER_BIT);
-
-   // Desenhar a linha x do eixo
-    glBegin(GL_LINES);
-
-      glColor3f(1.0, 1.0, 1.0);
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(360.0, 0.0, 0.0);
-
-   glEnd();
-
-   // Desenhar a linha y do eixo
-   glBegin(GL_LINES);
-
-      glColor3f(1.0, 1.0, 1.0);
-      glVertex3f(0.0, -1.0, 0.0);
-      glVertex3f(0.0, 1.0, 0.0);
-
-   glEnd();
-   
-
-      
-   for(float x = 0; x < 360; x = x + 2)
-   {
-      glBegin(GL_LINE_STRIP);
-
-         float y = sin(x*((3.1415)/180));        
-         glColor3f(0.0, 1.0, 0.0);
-         glVertex2f(x, y);
-         glVertex2f((x+2),sin((x+2)*((3.1415)/180)));        
-         
+         glColor3f(1.0, 1.0, 1.0);
+         glVertex3f(0.0, 0.0, 0.0);
+         glVertex3f(360.0, 0.0, 0.0);
 
       glEnd();
 
+      // Desenhar a linha y do eixo
+      glBegin(GL_LINES);
+
+         glColor3f(1.0, 1.0, 1.0);
+         glVertex3f(0.0, -1.0, 0.0);
+         glVertex3f(0.0, 1.0, 0.0);
+
+      glEnd();
+
+
+
+      for(float x = 0; x < 360; x = x + 60)
+      {  
+         glBegin(GL_LINE_STRIP);
+
+            float y = sin(x*((3.1415)/180));        
+            glColor3f(0.0, 1.0, 0.0);
+            glVertex2f(x, y);
+            glVertex2f((x+60),sin((x+60)*((3.1415)/180)));
+
+         
+         
+         glEnd();
+
       
 
       glutSwapBuffers ();
       
+      }
+
+       break;
+
+      case 'b':
+
+      // Limpar todos os pixels
+      glClear (GL_COLOR_BUFFER_BIT);
+
+      // Desenhar a linha x do eixo
+      glBegin(GL_LINES);
+
+         glColor3f(1.0, 1.0, 1.0);
+         glVertex3f(0.0, 0.0, 0.0);
+         glVertex3f(360.0, 0.0, 0.0);
+
+      glEnd();
+
+      // Desenhar a linha y do eixo
+      glBegin(GL_LINES);
+
+         glColor3f(1.0, 1.0, 1.0);
+         glVertex3f(0.0, -1.0, 0.0);
+         glVertex3f(0.0, 1.0, 0.0);
+
+      glEnd();
+   
+
+      // Desenhar gráfico senoidal em loop   
+      for(float x = 0; x < 360; x = x + 30)
+      {
+         glBegin(GL_LINE_STRIP);
+
+            float y = sin(x*((3.1415)/180));        
+            glColor3f(0.0, 1.0, 0.0);
+            glVertex2f(x, y);
+            glVertex2f((x+30),sin((x+30)*((3.1415)/180)));
+
+         
+         
+         glEnd();
+
+      
+
+      glutSwapBuffers ();
+
+      } 
+
+      break;
+
+      case 'c':
+
+      // Limpar todos os pixels
+      glClear (GL_COLOR_BUFFER_BIT);
+
+      // Desenhar a linha x do eixo
+      glBegin(GL_LINES);
+
+         glColor3f(1.0, 1.0, 1.0);
+         glVertex3f(0.0, 0.0, 0.0);
+         glVertex3f(360.0, 0.0, 0.0);
+
+      glEnd();
+
+      // Desenhar a linha y do eixo
+      glBegin(GL_LINES);
+
+         glColor3f(1.0, 1.0, 1.0);
+         glVertex3f(0.0, -1.0, 0.0);
+         glVertex3f(0.0, 1.0, 0.0);
+
+      glEnd();
+   
+
+      
+      for(float x = 0; x < 360; x = x + 2)
+      {
+         glBegin(GL_LINE_STRIP);
+
+            float y = sin(x*((3.1415)/180));        
+            glColor3f(0.0, 1.0, 0.0);
+            glVertex2f(x, y);
+            glVertex2f((x+2),sin((x+2)*((3.1415)/180)));        
+         
+
+         glEnd();
+
+      
+
+      glutSwapBuffers ();     
+
+      }
+
+      break;
+
    }
 
-  
-
 }
+   
+   
+
+   
 
 void init (void)
 {
@@ -195,7 +196,7 @@ void init (void)
 
    glMatrixMode(GL_MODELVIEW); // Select The Modelview Matrix
    glLoadIdentity();           // Inicializa com matriz identidade
-}
+}  
 
 
 void keyboard(unsigned char key, int x, int y)
@@ -206,7 +207,9 @@ void keyboard(unsigned char key, int x, int y)
       case 'a':
          
          //glutDisplayFunc(display_6);
-         glutIdleFunc(display_6);
+         var = 'a';
+         glutPostRedisplay();
+
 
          break;
 
@@ -215,7 +218,9 @@ void keyboard(unsigned char key, int x, int y)
 
          
          //glutDisplayFunc(display_12);
-         glutIdleFunc(display_12);
+         var = 'b';
+         glutPostRedisplay();
+
          
       break;
 
@@ -223,7 +228,9 @@ void keyboard(unsigned char key, int x, int y)
       case 'd':
          
          //glutDisplayFunc(display_180);
-         glutIdleFunc(display_180);
+         var = 'c';
+         glutPostRedisplay();
+
       break;
 
      
