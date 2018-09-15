dfde7353459b89d03b33286ce5655179fa35a534
Fábio Júnior <33606799+JuniorPenguim@users.noreply.github.com>
2018-09-15 09:01:55 -0300

    Exercicio_dois
    Correção de linhas desnecessárias

--- Exercicio_dois.cpp
+++ Exercicio_dois.cpp
@@ -8,19 +8,29 @@ void init (void);
 void keyboard(unsigned char key, int x, int y);
 char var;
 
+void desenhaEixos()
+{
+      glBegin(GL_LINES);
+      glColor3f (1.0, 1.0, 1.0);
+      glVertex3f(-10.0, 0.0, 0.0);
+      glVertex3f( 360.0, 0.0, 0.0);
+      glVertex3f(0.0, -10.0, 0.0);
+      glVertex3f(0.0,  10.0, 0.0);
+   glEnd();
+
+}
+
 int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
    glutInitWindowSize (1920, 1080);
    glutInitWindowPosition (100, 100);
-   glutCreateWindow ("Exercicio 2 Test");
+   glutCreateWindow ("Exercicio 2");
    init ();
+   printf("Pressione as teclas 'a', 's' e 'd' para alternar entre as formas da senoidal. \n");
    glutDisplayFunc(display);
    glutKeyboardFunc(keyboard);
-   
-   
-
 
    printf("Pressione ESC para fechar.\n");
 
@@ -35,7 +45,6 @@ int main(int argc, char** argv)
 void display(void)
 {
 
-
    switch(var)
    {
       case 'a':
@@ -43,72 +52,34 @@ void display(void)
       // Limpar todos os pixels
       glClear (GL_COLOR_BUFFER_BIT);
 
-      // Desenhar a linha x do eixo
-      glBegin(GL_LINES);
-
-         glColor3f(1.0, 1.0, 1.0);
-         glVertex3f(0.0, 0.0, 0.0);
-         glVertex3f(360.0, 0.0, 0.0);
-
-      glEnd();
-
-      // Desenhar a linha y do eixo
-      glBegin(GL_LINES);
-
-         glColor3f(1.0, 1.0, 1.0);
-         glVertex3f(0.0, -1.0, 0.0);
-         glVertex3f(0.0, 1.0, 0.0);
-
-      glEnd();
-
-
+      desenhaEixos();
 
       for(float x = 0; x < 360; x = x + 60)
       {  
+         //desenho do gráfico senoidal dividido em 6 partes
          glBegin(GL_LINE_STRIP);
 
             float y = sin(x*((3.1415)/180));        
             glColor3f(0.0, 1.0, 0.0);
             glVertex2f(x, y);
-            glVertex2f((x+60),sin((x+60)*((3.1415)/180)));
-
+            glVertex2f((x+60),sin((x+60)*((3.1415)/180)));         
          
-         
-         glEnd();
-
-      
+         glEnd();      
 
       glutSwapBuffers ();
       
       }
 
-       break;
+      break;
 
       case 'b':
 
       // Limpar todos os pixels
       glClear (GL_COLOR_BUFFER_BIT);
 
-      // Desenhar a linha x do eixo
-      glBegin(GL_LINES);
+      desenhaEixos();   
 
-         glColor3f(1.0, 1.0, 1.0);
-         glVertex3f(0.0, 0.0, 0.0);
-         glVertex3f(360.0, 0.0, 0.0);
-
-      glEnd();
-
-      // Desenhar a linha y do eixo
-      glBegin(GL_LINES);
-
-         glColor3f(1.0, 1.0, 1.0);
-         glVertex3f(0.0, -1.0, 0.0);
-         glVertex3f(0.0, 1.0, 0.0);
-
-      glEnd();
-   
-
-      // Desenhar gráfico senoidal em loop   
+      // desenho do grafico senoidal dividido em 12 partes
       for(float x = 0; x < 360; x = x + 30)
       {
          glBegin(GL_LINE_STRIP);
@@ -116,13 +87,9 @@ void display(void)
             float y = sin(x*((3.1415)/180));        
             glColor3f(0.0, 1.0, 0.0);
             glVertex2f(x, y);
-            glVertex2f((x+30),sin((x+30)*((3.1415)/180)));
-
+            glVertex2f((x+30),sin((x+30)*((3.1415)/180)));         
          
-         
-         glEnd();
-
-      
+         glEnd();      
 
       glutSwapBuffers ();
 
@@ -135,26 +102,9 @@ void display(void)
       // Limpar todos os pixels
       glClear (GL_COLOR_BUFFER_BIT);
 
-      // Desenhar a linha x do eixo
-      glBegin(GL_LINES);
-
-         glColor3f(1.0, 1.0, 1.0);
-         glVertex3f(0.0, 0.0, 0.0);
-         glVertex3f(360.0, 0.0, 0.0);
-
-      glEnd();
-
-      // Desenhar a linha y do eixo
-      glBegin(GL_LINES);
-
-         glColor3f(1.0, 1.0, 1.0);
-         glVertex3f(0.0, -1.0, 0.0);
-         glVertex3f(0.0, 1.0, 0.0);
-
-      glEnd();
-   
-
+      desenhaEixos();
       
+      //desenho gráfico senoidal dividido emm 180 partes
       for(float x = 0; x < 360; x = x + 2)
       {
          glBegin(GL_LINE_STRIP);
@@ -162,12 +112,9 @@ void display(void)
             float y = sin(x*((3.1415)/180));        
             glColor3f(0.0, 1.0, 0.0);
             glVertex2f(x, y);
-            glVertex2f((x+2),sin((x+2)*((3.1415)/180)));        
-         
+            glVertex2f((x+2),sin((x+2)*((3.1415)/180)));         
 
-         glEnd();
-
-      
+         glEnd();      
 
       glutSwapBuffers ();     
 
@@ -177,11 +124,7 @@ void display(void)
 
    }
 
-}
-   
-   
-
-   
+}  
 
 void init (void)
 {
@@ -197,7 +140,6 @@ void init (void)
    glLoadIdentity();           // Inicializa com matriz identidade
 }  
 
-
 void keyboard(unsigned char key, int x, int y)
 {
    switch (key)
@@ -205,7 +147,6 @@ void keyboard(unsigned char key, int x, int y)
       case 'A':
       case 'a':
          
-         //glutDisplayFunc(display_6);
          var = 'a';
          glutPostRedisplay();
 
@@ -213,10 +154,8 @@ void keyboard(unsigned char key, int x, int y)
          break;
 
       case 'S':
-      case 's':
-
-         
-         //glutDisplayFunc(display_12);
+      case 's':      
+        
          var = 'b';
          glutPostRedisplay();
 
@@ -224,9 +163,8 @@ void keyboard(unsigned char key, int x, int y)
       break;
 
       case 'D':
-      case 'd':
+      case 'd':         
          
-         //glutDisplayFunc(display_180);
          var = 'c';
          glutPostRedisplay();
 
@@ -236,7 +174,6 @@ void keyboard(unsigned char key, int x, int y)
       case 27:
         exit(0);
       break;
-
 
    }
 }
