3eb2e4f93c44859bf5f06909fc6a601c0b258847
Fábio Júnior <33606799+JuniorPenguim@users.noreply.github.com>
2018-09-26 21:30:48 -0300

    Exercicio_doze

--- Exercicio_doze.cpp
+++ Exercicio_doze.cpp
@@ -9,6 +9,10 @@ void init (void);
 void desenhaTabuleiro();
 void idle();
 
+float rotationX = 25.0, rotationY = 0.0;
+int   last_x, last_y;
+int width = 640, height = 480;
+
 float desiredFPS = 30;
 int varx = -80;
 int vary = -80;
@@ -16,6 +20,9 @@ float dirx = 1.0;
 float diry = 1.0;
 int positx = -80;
 int posity = -80;
+
+int distOrigem = 200;
+
 
 
 void idle()
@@ -51,8 +58,6 @@ void idle()
          vary = vary + diry * 1;
      }
     
-
-
    
 
     /* Update tLast for next time, using static local variable */
@@ -62,13 +67,11 @@ void idle()
 
 }
 
-
-
 void init (void)
 {
 
    glClearColor (0.0, 0.0, 0.0, 0.0);
-   
+   glShadeModel (GL_SMOOTH);   
 
    glEnable(GL_LIGHT0);                   // habilita luz 0
    glEnable(GL_COLOR_MATERIAL);           // Utiliza cor do objeto como material
@@ -81,49 +84,60 @@ void init (void)
    
 }
 
+void reshape (int w, int h)
+{
+   width = w;
+   height = h;
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+}
 
 void display(void)
 {
-   // Limpar todos os pixels
-   glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-   //glLoadIdentity(); // Inicializa com matriz identidade
 
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   gluPerspective(60, 1.333, 1, 100);
-   //glOrtho(-100.0, 100.0, -100.0, 100.0, -100.0, 100.0);
-
-
-
-   desenhaTabuleiro();
+   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   gluPerspective(60.0, (GLfloat) width/(GLfloat) height, 1.0, 400.0);
+   //glOrtho(-100.0, 100.0, -100.0, 100.0, -100.0, 100.0); // visão em 2D
 
    glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity ();   
+   glLoadIdentity ();
+   gluLookAt (0.0, 0.0, distOrigem, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-   gluLookAt (0.0, 0.0, 100.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-   //gluLookAt (0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); 
+   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+   glColor3f (1, 1, 1);
+   desenhaTabuleiro();
+   glColor3f (1, 0, 0);
+   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
    
-   
-    
    glPushMatrix();
-
-   glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-
-      glColor3f(1.0, 0.0, 0.0);
+      glRotatef(-45, 1, 0, 0);
       glTranslatef(varx, vary,  0.0);
       glutSolidCube(40);
-
    glPopMatrix();
 
-
-     
-
-
-
-   glutSwapBuffers ();
-   glutPostRedisplay();
+   glutSwapBuffers();
 
    
+}
+
+void keyboard (unsigned char key, int x, int y)
+{
+   switch (tolower(key))
+   {
+      case '+' :
+         distOrigem--;
+         if(distOrigem<2) distOrigem=2;
+      break;
+      case '-' :
+         distOrigem++;
+         if(distOrigem>5) distOrigem=5;
+      break;
+      case 27:
+         exit(0);
+      break;
+   }
 }
 
 void specialKeys(int key, int x, int y)
@@ -174,22 +188,43 @@ void specialKeys(int key, int x, int y)
 }
    
 
+// Motion callback
+void motion(int x, int y )
+{
+   rotationX += (float) (y - last_y);
+   rotationY += (float) (x - last_x);
+
+   last_x = x;
+   last_y = y;
+}
+
+// Mouse callback
+void mouse(int button, int state, int x, int y)
+{
+   if ( button == GLUT_LEFT_BUTTON && state == GLUT_DOWN )
+   {
+      last_x = x;
+      last_y = y;
+   }
+}
+
 
 int main(int argc, char** argv)
 {
-   
    glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_DOUBLE|GLUT_DEPTH|GLUT_RGB);
-   glutInitWindowSize (640, 480);
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+   glutInitWindowSize (width, height);
    glutInitWindowPosition (100, 100);
-   glutCreateWindow ("Tabuleiro 3D");
+   glutCreateWindow (argv[0]);
    glutSpecialFunc( specialKeys );
-   glutIdleFunc( idle);   
-   init ();   
+   glutIdleFunc(idle);
+   init ();
    glutDisplayFunc(display);
-   
+   glutReshapeFunc(reshape);   
+   glutMouseFunc( mouse );
+   glutMotionFunc( motion );
+   glutKeyboardFunc(keyboard);   
    glutMainLoop();
-
    return 0;
 }
 
@@ -205,20 +240,23 @@ void desenhaTabuleiro()
    for(float j = -100;j < 100 ; j = j + 40)
    {
 
-      glColor3f(1.0, 1.0, 1.0);
-      glBegin(GL_POLYGON);
-      
-         glVertex2f (i,j);
-         glVertex2f (i+40,j);
-         glVertex2f (i+40,j+40);
-         glVertex2f (i,j+40);
+      glPushMatrix();
 
-      glEnd();
+         glRotatef(-45, 1, 0, 0);
+
+         glBegin(GL_POLYGON);
+         
+            glVertex2f (i,j);
+            glVertex2f (i+40,j);
+            glVertex2f (i+40,j+40);
+            glVertex2f (i,j+40);
+
+         glEnd();
+
+      glPopMatrix();
    }
 
   }    
 
 }
 
-
-   
